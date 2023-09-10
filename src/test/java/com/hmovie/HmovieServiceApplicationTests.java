a494d78a8709cb0caa0500a537de908ff075a3ef
hugh.huynh <hugh.huynh@pal.net.vn>
2023-09-10 11:27:53 +0700

    # Init

---
+++ src/test/java/com/hmovie/HmovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmovieServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
